0ba4563584bd266607d6e0a6fa9f75cbd6bb8bbb
Seungil <seungilbang@khu.ac.kr>
2023-04-25 19:51:18 +0900

    [UPD] CORS 문제 해결

--- server/src/main/java/com/web/myreview/config/auth/SecurityConfig.java
+++ server/src/main/java/com/web/myreview/config/auth/SecurityConfig.java
@@ -3,14 +3,23 @@ package com.web.myreview.config.auth;
 import com.web.myreview.domain.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
+@Configuration // <- 추가했더니 .cors().configurationSource(corsConfigurationSource()) 에러 안뜸
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig  {
@@ -20,9 +29,11 @@ public class SecurityConfig  {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable().headers().frameOptions().disable()
+                .cors().configurationSource(corsConfigurationSource())
                 .and()
+                .csrf().disable()
                 .formLogin().disable()
+                .httpBasic().disable()
                 .authorizeHttpRequests(authz -> authz
                         .anyRequest().permitAll()
                 )
@@ -34,9 +45,20 @@ public class SecurityConfig  {
                 .oauth2Login().userInfoEndpoint().userService(customOAuth2UserService);
 
         return http.build();
-
-
-
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PATCH", "DELETE","OPTIONS"));
+        configuration.setAllowCredentials(true);
+        configuration.addExposedHeader("Authorization");
+        configuration.addExposedHeader("Refresh");
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
